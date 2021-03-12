7302b5a9553d7fba8258e6f71ca3ba920b588f11
Sebastien Vezina <seblife@pm.me>
2021-03-11 22:38:41 -0300

    Added comments to JavaScript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+/*************************************************************
+  * Allows the frontend to interact with the smart contract 
+  * on the blockchain using Web3.js 
+  ************************************************************/
+
 var web3 = new Web3(Web3.givenProvider);
 var accountAddress;
 var contractInstance;
 var contractAddress = "0x57Ac66F98dc9C11289B961CDbAae0BC48330872a";  
 var contractURL = `https://rinkeby.etherscan.io/address/${contractAddress}`;
 
+// Initialize the Page and load Metamask
 $(document).ready(function() {
   enableEthereum();
   $("#bet_button").click(placeBet);
@@ -15,6 +21,17 @@ $(document).ready(function() {
   showResultDice(0);
 });
 
+// Activate Metamask and Initialize the Contract Instance
+function enableEthereum() {
+  window.ethereum.enable()
+  .then(function(accounts) {
+    window.accountAddress = accounts[0];
+    window.contractInstance = new web3.eth.Contract(window.abi, window.contractAddress, {from: accounts[0]});
+    refreshBalances();
+  });
+}
+
+// Update the Result Dice Image
 // 0 = No result yet [?], 1-6 shows corresponding dice, -1 = animated dice
 function showResultDice(dice) {
   if(dice >= 0) {
@@ -25,22 +42,19 @@ function showResultDice(dice) {
   }
 }
 
+// Updates all the balances on the page
+// 1. Account Balance
+// 2. Contract Balance
+// 3. Unclaimed Wins
+// 4. Cahinlink Balance
 function refreshBalances(){
     displayETHBalance(accountAddress, "AccountBalanceText", "Account Balance: ");
     displayETHBalance(contractAddress, "ContractBalanceText", "Contract Balance: ");
     getWithdrawableWins();
     getChainLinkBalance();
 }
-  
-function enableEthereum() {
-  window.ethereum.enable()
-  .then(function(accounts) {
-    window.accountAddress = accounts[0];
-    window.contractInstance = new web3.eth.Contract(window.abi, window.contractAddress, {from: accounts[0]});
-    refreshBalances();
-  });
-}
 
+// Allow user to withdraw his unclaimed wins to his wallet
 function getWithdrawableWins() {
   contractInstance.methods.getUnclaimedWins().call()
   .then(result => {
@@ -49,6 +63,7 @@ function getWithdrawableWins() {
   });
 }
 
+// Get the LINK balance for this contract
 function getChainLinkBalance() {
   contractInstance.methods.getChainLinkBalance().call()
   .then(result => {
@@ -56,6 +71,7 @@ function getChainLinkBalance() {
   });
 }
 
+// Allow user to withdraw his unclaimed wins in the contract
 async function withdrawWins() {
   new Audio('sounds/click2.mp3').play();
   let amount = await contractInstance.methods.getUnclaimedWins().call();
@@ -89,6 +105,7 @@ async function withdrawWins() {
   }
 }
 
+// Place the bet on the smart contract
 function placeBet(){ 
 
   new Audio('sounds/click2.mp3').play();
@@ -166,13 +183,15 @@ function placeBet(){
     .on('error', console.error); 
 }
 
+// Print a new message in the application message box
 function printMessage(message) {
   document.getElementById("Messages").innerHTML = message;
 }
 
-function displayETHBalance(account, elementId, prefix) {
+// Get the ETH balance for an address and display it on the 'elementId' on the page
+function displayETHBalance(address, elementId, prefix) {
     try {
-        web3.eth.getBalance(account, function (error, wei) {
+        web3.eth.getBalance(address, function (error, wei) {
             if (!error) {
                 let balance = web3.utils.fromWei(wei, 'ether');
                 document.getElementById(elementId).innerHTML = `${prefix} ${balance} ETH`;
@@ -181,17 +200,17 @@ function displayETHBalance(account, elementId, prefix) {
     } catch (err) {
         document.getElementById(elementId).innerHTML = err;
     }
+}
+
+// Returns true if the string str is a positive integer
+function isNormalInteger(str) {
+  str = str.trim();
+  if (!str) {
+      return false;
   }
-  
-  // Returns true if the string str is a positive integer
-  function isNormalInteger(str) {
-    str = str.trim();
-    if (!str) {
-        return false;
-    }
-    str = str.replace(/^0+/, "") || "0";
-    let n = Math.floor(Number(str));
-    return n !== Infinity && String(n) === str && n >= 0;
-  }
-  
+  str = str.replace(/^0+/, "") || "0";
+  let n = Math.floor(Number(str));
+  return n !== Infinity && String(n) === str && n >= 0;
+}
+
   
